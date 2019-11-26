52cefa212dd7b1e3c0448854ef07d3b7aff38cd0
Ester Cristina <ester.cristina.silva@accenture.com>
2019-11-26 10:05:20 -0300

    Finalization of the back part

--- src/server.js
+++ src/server.js
@@ -3,15 +3,38 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 require('./models/User');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
 
 mongoose.connect('mongodb+srv://omnistackTeh:Teh@omnistackteh-t55ny.mongodb.net/semana09?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+});
+
+
+const connetedUsers = {}; 
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connetedUsers[user_id] = socket.id;
+});
+
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connetedUsers = connetedUsers;
+
+    return next();
 });
 
 //GET, POST, PUT, DELETE
@@ -24,4 +47,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333); 
+server.listen(3333); 
